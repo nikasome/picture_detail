56059ec8295198b35e99a66c186be168143ae0c3
nikasome <kou1pomu@yahoo.co.jp>
2019-12-16 04:25:12 +0900

    add - cut image by rect

--- src/App.js
+++ src/App.js
@@ -4,49 +4,71 @@ import './App.css';
 import testImage from './picture/yuushou.png';
 
 const style = {
-  border: '1px solid gray',
-  backgroundColor: 'white'
+  border: '1px solid gray'
 };
 
 function Canvas() {
   const isFirstLoad = useRef(true);
-  const drawing = useRef(false); //描画には直接関係しないのでuseRef
+  const drawing = useRef(false); //State更新には直接関係しないのでuseRef
+  // 画像と図形のcanvasを分ける
+  const imageRef = useRef();
   const canvasRef = useRef();
   const [start, setStart] = useState([0, 0]);
   const [end, setEnd] = useState([0, 0]);
 
   useEffect(() => {
+    // 初回のみcanvasの背景に画像をセット
     if (isFirstLoad.current) {
       isFirstLoad.current = false;
+
       const backImage = new Image();
       backImage.src = testImage;
       console.log(backImage); //<img src="..." />が出力される
+      // 画像読み込みまで時間が掛かるためonloadを付ける
       backImage.onload = () => {
         //console.log(backImage);
         console.log(backImage.naturalWidth, backImage.naturalHeight);
+        imageRef.current.width = backImage.naturalWidth;
+        imageRef.current.height = backImage.naturalHeight;
         canvasRef.current.width = backImage.naturalWidth;
         canvasRef.current.height = backImage.naturalHeight;
-        const ctx = getContext();
-        ctx.drawImage(
+        const image_ctx = getContext(imageRef);
+        image_ctx.drawImage(
           backImage,
           0,
           0,
           canvasRef.current.width,
           canvasRef.current.height
         );
+
+        /*
+        const rect_ctx = getContext();
+        rect_ctx.fillStyle = 'rgba(216, 216, 216, 0.5)';
+        rect_ctx.fillRect(
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height
+        );
+        */
       };
     }
     //console.log('moved useEffect');
   });
 
-  function getContext() {
-    return canvasRef.current.getContext('2d');
+  function getContext(ref = canvasRef) {
+    const ctx = ref.current.getContext('2d');
+    ctx.lineWidth = 3;
+    return ctx;
   }
 
   function startDrawing(x, y) {
     drawing.current = true;
     const ctx = getContext();
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    ctx.strokeRect(x, y, 10, 10);
     setStart([x, y]);
+    setEnd([10, 10]);
   }
 
   function draw(x, y) {
@@ -61,22 +83,27 @@ function Canvas() {
 
   function endDrawing() {
     drawing.current = false;
+    const ctx = getContext();
+    ctx.fillStyle = 'rgba(216, 216, 216, 0.9)';
+    ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    ctx.clearRect(start[0], start[1], end[0], end[1]);
+    ctx.strokeRect(start[0], start[1], end[0], end[1]);
   }
 
   return (
-    <canvas
-      id="canvas"
-      ref={canvasRef}
-      width="500px"
-      height="500px"
-      onMouseDown={e =>
-        startDrawing(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
-      }
-      onMouseUp={() => endDrawing()}
-      onMouseLeave={() => endDrawing()}
-      onMouseMove={e => draw(e.nativeEvent.offsetX, e.nativeEvent.offsetY)}
-      style={style}
-    />
+    <div className="canvas-wrapper">
+      <canvas ref={imageRef} style={style} />
+      <canvas
+        ref={canvasRef}
+        onMouseDown={e =>
+          startDrawing(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
+        }
+        onMouseUp={() => endDrawing()}
+        onMouseLeave={() => console.log('MouseLeaved')}
+        onMouseMove={e => draw(e.nativeEvent.offsetX, e.nativeEvent.offsetY)}
+        style={style}
+      />
+    </div>
   );
 }
 
